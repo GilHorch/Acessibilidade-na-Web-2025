9339283cb656afec42ca310f6a0dbe4164a24b23
Prof. Gilmar Horchulhak <108031801+GilHorch@users.noreply.github.com>
2025-04-15 15:58:54 -0300

    1

--- script.js
+++ script.js
@@ -28,3 +28,6 @@ alternaContraste.addEventListener('click',function(){
 })
 })
 ScrollReveal().reveal('#inicio', { delay: 500 });
+ScrollReveal().reveal('#tropicalia', { delay: 500 });
+ScrollReveal().reveal('#galeria', { delay: 500 });
+ScrollReveal().reveal('#contato', { delay: 500 });
